67f70c336de9b9ddbe380df0000c6085015fa430
Yasiel <yamalvarez89@gmail.com>
2025-06-11 14:21:15 -0400

    Create StudentDTO.java,

--- src/main/java/com/yasiel/studentapi/StudentDTO.java
+++ src/main/java/com/yasiel/studentapi/StudentDTO.java
@@ -3,7 +3,11 @@ package com.yasiel.studentapi;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class StudentDTO {
 
     @NotBlank(message = "Name is required")
@@ -12,30 +16,6 @@ public class StudentDTO {
     @Min(value = 1, message = "Grade must be at least 1")
     @Max(value = 12, message = "Grade must be at most 12")
     private int grade;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getGrade() {
-        return grade;
-    }
-
-    public void setGrade(int grade) {
-        this.grade = grade;
-    }
-
-    public double getScore() {
-        return score;
-    }
-
-    public void setScore(double score) {
-        this.score = score;
-    }
 
     @Min(value = 0, message = "Score must be at least 0")
     @Max(value = 100, message = "Score must be at most 100")
